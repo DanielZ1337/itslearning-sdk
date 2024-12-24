0ecd344f8f6fc74221c65c439fab9f97e2a3f824
Daniel Bermann Schmidt <danielz2nd@hotmail.com>
2024-12-24 03:13:11 +0100

    export types together with main sdk, generate index.ts for types with barrelsby

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  outDir: "dist",
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  dts: true,
-});
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    outDir: "dist",
+    format: ["cjs", "esm"],
+    sourcemap: true,
+    clean: true,
+    dts: true,
+    splitting: false,
+  },
+  {
+    entry: ["src/types/index.ts"],
+    outDir: "dist/types",
+    format: ["esm"],
+    sourcemap: true,
+    clean: true,
+    dts: true,
+    splitting: false,
+  },
+]);
