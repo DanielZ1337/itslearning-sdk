5992c59e64b5c282eba468ed69a82bce16c18d0b
Daniel Bermann Schmidt <danielz2nd@hotmail.com>
2024-12-24 02:14:07 +0100

    fix import error, add tsup for bundling

--- src/types/api/native/Itslearning.RestApi.Entities.ThreadParticipant.ts
+++ src/types/api/native/Itslearning.RestApi.Entities.ThreadParticipant.ts
@@ -1,18 +1,18 @@
-import type { ItsolutionsItslUtilsConstantsHierarchyRole } from "../Itsolutions.ItslUtils.Constants.HierarchyRole.ts";
 import type { ItslearningRestApiEntitiesLocationRecipientType } from "./Itslearning.RestApi.Entities.LocationRecipientType.ts";
+import type { ItsolutionsItslUtilsConstantsHierarchyRole } from "./Itsolutions.ItslUtils.Constants.HierarchyRole.ts";
 
 export type ItslearningRestApiEntitiesThreadParticipant = {
-	SourceLocationId: number;
-	SourceLocationType: ItslearningRestApiEntitiesLocationRecipientType;
-	IsDeleted: boolean;
-	CanReceive: boolean;
-	RoleInHierarchy: ItsolutionsItslUtilsConstantsHierarchyRole;
-	PersonId: number;
-	FirstName: string;
-	LastName: string;
-	FullName: string;
-	ProfileUrl: string;
-	AdditionalInfo: string;
-	ProfileImageUrl: string;
-	ProfileImageUrlSmall: string;
+  SourceLocationId: number;
+  SourceLocationType: ItslearningRestApiEntitiesLocationRecipientType;
+  IsDeleted: boolean;
+  CanReceive: boolean;
+  RoleInHierarchy: ItsolutionsItslUtilsConstantsHierarchyRole;
+  PersonId: number;
+  FirstName: string;
+  LastName: string;
+  FullName: string;
+  ProfileUrl: string;
+  AdditionalInfo: string;
+  ProfileImageUrl: string;
+  ProfileImageUrlSmall: string;
 };
